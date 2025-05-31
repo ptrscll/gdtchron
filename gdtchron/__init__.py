8cdb4dec0862e604b566b6cc4091f5cd3c0146d1
Dylan Vasey <78242242+dyvasey@users.noreply.github.com>
2025-05-31 19:03:39 -0400

    Add  basic plotting functions (#3)
    * Add plot_vtk_2d function
    
    * Add Pyvista headless display
    
    * Add add_comp_field function
    
    * Update all functions to add whitespace after commas
    
    * Update Ruff settings to fix D417 behavior

--- gdtchron/__init__.py
+++ gdtchron/__init__.py
@@ -1,1 +1,12 @@
-"""Placeholder docstring."""
+"""GDTchron.
+
+GDTchron is a Python package for using the outputs of geodynamic models to predict
+thermochronometric ages.
+
+Modules:
+    visualization: Tools for visualizing VTK files and model thermochronometric ages.
+"""
+
+from gdtchron.visualization import add_comp_field, plot_vtk_2d
+
+__all__ = ["plot_vtk_2d", "add_comp_field"]
