4c25e8c0b67e30ed90b786e59fa85ba2fe476d8e
Peter Scully <103226047+ptrscll@users.noreply.github.com>
2025-06-08 14:45:53 -0400

    Add apatite fission track module (#5)
    * Modify linter for initial aft code
    
    Made T a valid variable name
    
    Update linter
    
    Update linter
    
    * Remove placeholder test
    
    * Add  basic plotting functions (#3)
    
    * Add plot_vtk_2d function
    
    * Add Pyvista headless display
    
    * Add add_comp_field function
    
    * Update all functions to add whitespace after commas
    
    * Update Ruff settings to fix D417 behavior
    
    Remove carve-outs for capitalized params
    
    Make Dpar an acceptable variable name
    
    * Add apatite fission track module
    
    Added g and constants
    
    add remaining annealing functions
    
    Update docstrings to match style of visualization.py
    
    Add remaining aft.py functions
    
    update aft.py to follow updated linter
    
    Update aft.py to comply with new linter
    
    Add Dpar conversion function
    
    Fix indentation to match numpy style
    
    Add r to rho conversion function
    
    Add function to calculate AFT age from reduced lengths
    
    Add remaining aft.py functions
    
    * Add aft tests
    
    Added tests for g
    
    add tests for remaining annealing functions
    
    Update style of tests
    
    Add remaining aft.py tests
    
    Add Dpar conversion test
    
    Add r to rho tests
    
    Add tests for age calculation
    
    Add remaining tests for aft.py
    
    Update parameter names
    
    Add remaining tests for aft.py
    
    * Add aft to gdtchron __init__

--- gdtchron/__init__.py
+++ gdtchron/__init__.py
@@ -7,6 +7,7 @@ Modules:
     visualization: Tools for visualizing VTK files and model thermochronometric ages.
 """
 
+from gdtchron import aft
 from gdtchron.visualization import add_comp_field, plot_vtk_2d
 
-__all__ = ["plot_vtk_2d", "add_comp_field"]
+__all__ = ["plot_vtk_2d", "add_comp_field", "aft"]
