a4dc291c653962fcdbd758a9b41209ff747b7db1
Peter Scully <ptrscll@gmail.com>
2025-06-10 13:46:08 -0400

    Finish tridiag_banded and add constants

--- gdtchron/he.py
+++ gdtchron/he.py
@@ -13,9 +13,27 @@ from scipy.interpolate import griddata
 from scipy.linalg import solve_banded
 from scipy.optimize import fsolve
 
+# Constants
+SYSTEM_PARAMS = {'AHe': {'freq_factor': 50e8 * 3.154e7,  # micrometers^2 / yr
+                         'activ_energy': 13800,        # J * mol^-1
+                         'R_238U': 18.81,  # Stopping distances (micrometers)
+                         'R_235U': 21.80,
+                         'R_232Th': 22.25},
+                 'ZHe': {'freq_factor': 0.46e8 * 3.154e7,  # micrometers^2 / yr
+                         'activ_energy': 169000,        # J * mol^-1
+                         'R_238U': 15.55,  # Stopping distances (micrometers)
+                         'R_235U': 18.05,
+                         'R_232Th': 18.43}}
 
-def tridiag_banded(a, b, c, diag_length):
+
+def tridiag_banded(a, b, c, diag_length, dtype=np.float32):
     """Set up tridiagonal matrix in banded form from values for the 3 diagonals.
+
+    For example, a tridiagonal matrix in banded form with values a, b, c for
+    its diagonals and with a principal diagonal of length 6 appears as follows:
+    [0, a, a, a, a, a]
+    [b, b, b, b, b, b]
+    [c, c, c, c, c, 0]
 
     Parameters
     ----------
@@ -27,6 +45,9 @@ def tridiag_banded(a, b, c, diag_length):
         Third diagonal value
     diag_length : float
         Length of principal diagonal 
+    dtype : type
+        Type of numbers in the matrix (default: np.float32). 32-bit floats are
+        preferred to save memory when running large numbers of forward models.
 
     Returns
     -------
@@ -34,8 +55,6 @@ def tridiag_banded(a, b, c, diag_length):
         Tridiagonal matrix
 
     """
-    dtype = np.float32
-    
     a_array = np.ones(diag_length, dtype=dtype) * a
     b_array = np.ones(diag_length, dtype=dtype) * b
     c_array = np.ones(diag_length, dtype=dtype) * c
@@ -43,7 +62,5 @@ def tridiag_banded(a, b, c, diag_length):
     banded_matrix = np.vstack((a_array, b_array, c_array))
     banded_matrix[0, 0] = 0
     banded_matrix[-1, -1] = 0
-    
-    # del a_array,b_array,c_array
 
     return banded_matrix
