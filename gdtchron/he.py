a8e067e680a12f55aaa33cd12010003006a1fecf
Peter Scully <ptrscll@gmail.com>
2025-06-11 12:24:10 -0400

    add quick calculate functions

--- gdtchron/he.py
+++ gdtchron/he.py
@@ -14,8 +14,11 @@ from scipy.linalg import solve_banded
 from scipy.optimize import fsolve
 
 # Constants
+
+# Frequency factors and activation energies are from Reiners and Brandon (2006)
+# and references therein. Stopping distances are from Ketcham et al. (2011).
 SYSTEM_PARAMS = {'AHe': {'freq_factor': 50e8 * 3.154e7,  # micrometers^2 / yr
-                         'activ_energy': 13800,        # J * mol^-1
+                         'activ_energy': 138000,        # J * mol^-1
                          'R_238U': 18.81,  # Stopping distances (micrometers)
                          'R_235U': 21.80,
                          'R_232Th': 22.25},
@@ -24,6 +27,9 @@ SYSTEM_PARAMS = {'AHe': {'freq_factor': 50e8 * 3.154e7,  # micrometers^2 / yr
                          'R_238U': 15.55,  # Stopping distances (micrometers)
                          'R_235U': 18.05,
                          'R_232Th': 18.43}}
+
+IDEAL_GAS_CONST = 8.3144598  # (J * K^-1 * mol^-1)
+U238_PER_U235 = 137.88  # Number of U-238 atoms for every U-235 atom (unitless)
 
 
 def tridiag_banded(a, b, c, diag_length, dtype=np.float32):
@@ -64,3 +70,86 @@ def tridiag_banded(a, b, c, diag_length, dtype=np.float32):
     banded_matrix[-1, -1] = 0
 
     return banded_matrix
+
+
+def calc_diffusivity(temperature, system):
+    """Calculate diffusivity from temperature and system diffusion parameters.
+    
+    After Reiners and Brandon (2006), with PV_a term assumed to be 0.
+
+    Parameters
+    ----------
+    temperature : float
+        Temperature (K)
+    system : string
+        String indicating whether to use parameters for the apatite system 
+        ('AHe') or the zircon system ('ZHe')
+
+    Returns
+    -------
+    kappa : float
+        Diffusivity (micrometers^2 / yr)
+
+    """
+    freq_factor = SYSTEM_PARAMS[system]['freq_factor']
+    activ_energy = SYSTEM_PARAMS[system]['activ_energy']
+
+    exponent = np.exp(-activ_energy / (IDEAL_GAS_CONST * temperature))
+    kappa = freq_factor * exponent
+
+    return kappa
+
+
+def calc_beta(diffusivity, node_spacing, time_interval):
+    """Calculate beta, a substitution term from Ketcham (2005).
+    
+    The equation uses diffusivity, the spacing of nodes within the modeled 
+    grain, and the timestep duration. It comes from the in-text equation
+    between equations 20 and 21 in Ketcham (2005).
+
+    Parameters
+    ----------
+    diffusivity : float
+        Diffusivity (micrometers^2 / yr)
+    node_spacing : float
+        Spacing of nodes in the modeled crystal (micrometers)
+    time_interval : float
+        Timestep in the thermal model (yr)
+
+    Returns
+    -------
+    beta : float
+        Beta (unitless), after Ketcham (2005).
+
+    """
+    beta = (2 * (node_spacing ** 2)) / (diffusivity * time_interval)
+    return beta
+
+
+def u_th_ppm_to_molg(u_ppm, th_ppm):
+    """Convert concentrations of U and Th from ppm to mol/g.
+
+    Parameters
+    ----------
+    u_ppm : float
+        U concentration (ppm)
+    th_ppm : float
+        Th concentration (ppm)
+
+    Returns
+    -------
+    u238_molg : float
+        U-238 (mol / g)
+    u235_molg : float
+        U-235 (mol / g)
+    th_molg : float
+        Th-232 (mol / g)
+    """
+    u238_ppm = (U238_PER_U235 / (1 + U238_PER_U235)) * u_ppm
+    u235_ppm = (1 / (1 + U238_PER_U235)) * u_ppm
+    
+    u238_molg = u238_ppm * 1e-6 / 238
+    u235_molg = u235_ppm * 1e-6 / 235
+    th_molg = th_ppm * 1e-6 / 232
+
+    return (u238_molg, u235_molg, th_molg)
