c6749896f9ab44902d77e6a8ca3b874b45b9d6f9
Peter Scully <ptrscll@gmail.com>
2025-06-18 12:37:22 -0400

    Add he module to __init__.py
    Fix style on __init__.py

--- gdtchron/__init__.py
+++ gdtchron/__init__.py
@@ -1,6 +1,6 @@
 """GDTchron - Geodynamic Thermochronology"""  # noqa: D400
 
-from gdtchron import aft
+from gdtchron import aft, he
 from gdtchron._visualization import add_comp_field, plot_vtk_2d
 
-__all__ = ["plot_vtk_2d", "add_comp_field", "aft"]
+__all__ = ["plot_vtk_2d", "add_comp_field", "aft", "he"]
