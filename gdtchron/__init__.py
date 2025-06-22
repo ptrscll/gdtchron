8117720d7578dda0d33eb8bc85168a4a57b177ec
Peter Scully <ptrscll@gmail.com>
2025-06-22 16:22:03 -0400

    Update init.py to include run_tt_paths

--- gdtchron/__init__.py
+++ gdtchron/__init__.py
@@ -1,6 +1,7 @@
 """GDTchron - Geodynamic Thermochronology"""  # noqa: D400
 
 from gdtchron import aft, he
+from gdtchron._parallel_vtk import run_tt_paths
 from gdtchron._visualization import add_comp_field, plot_vtk_2d
 
-__all__ = ["plot_vtk_2d", "add_comp_field", "aft", "he"]
+__all__ = ["plot_vtk_2d", "add_comp_field", "aft", "he", "run_tt_paths"]
